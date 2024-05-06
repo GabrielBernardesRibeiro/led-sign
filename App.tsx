6b089c38816cb3d06493fd86bd0756d8cd73c407
gabriel bernardes ribeiro <gabriel.bernardes.ribeiro@hotmail.com>
2024-05-06 09:54:20 -0300

    update and add expo updates

--- App.tsx
+++ App.tsx
@@ -2,15 +2,34 @@ import {
   PaperProvider,
   MD3DarkTheme as DefaultTheme,
 } from "react-native-paper";
-import Main from "./src/Main";
 import { TextProvider } from "./src/context/Text.context";
 import EntryPoint from "./src/entry-point";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
 
 const theme = {
   ...DefaultTheme,
 };
 
 export default function App() {
+  const onFetchUpdateAsync = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
   return (
     <PaperProvider theme={theme}>
       <TextProvider>
